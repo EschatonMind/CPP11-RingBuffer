fd89c01b9115d2ab3065c5ab79d6d80c07c89b55
Punchline <re70977za@outlook.com>
2022-08-10 19:13:15 +0430

    expanding on buffer

--- RingBuffer.h
+++ RingBuffer.h
@@ -4,9 +4,40 @@ using namespace std;
 template<class T>
 class RingBuffer
 {
+private:
+	int m_pose;
+	int m_size;
+	T *m_values;
 
 public:
 	class Iterator;
+
+public:
+	RingBuffer(int size) : m_pose(0), m_size(size), m_values(NULL) {
+		m_values = new T[size];
+	}
+	
+	~RingBuffer() {
+		delete [] m_values;
+
+	}
+
+	int size() {
+		return m_size;
+	}
+
+	void add(T value) {
+
+		m_values[m_pose++] = value;
+
+		if (m_pose == m_size) {
+			m_pose = 0;
+		}
+	}
+
+	T& get(int pose) {
+		return m_values[pose];
+	}
 };
 
 template<class T>
--- Source.cpp
+++ Source.cpp
@@ -5,5 +5,18 @@ int main() {
 	RingBuffer<int>::Iterator it;
 	it.print();
 
+	cout << endl;
+
+	RingBuffer<int> Buffer1(3);
+	Buffer1.add(1);
+	Buffer1.add(3);
+	Buffer1.add(2);
+	Buffer1.add(4);
+	for (int i = 0; i != Buffer1.size(); i++) {
+		cout << Buffer1.get(i);
+	}
+
+	cout << endl;
+
 	return 0;
 }
