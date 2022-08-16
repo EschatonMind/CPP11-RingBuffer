d9da0ca6653aaa1fe0921f0b2ccdac23d8d06882
Punchline <re70977za@outlook.com>
2022-08-16 17:16:35 +0430

    adding comments

--- RingBuffer.h
+++ RingBuffer.h
@@ -1,39 +1,46 @@
 #pragma once
 #include <iostream>
 using namespace std;
+// declareing the " RingBuffer Class " from template
 template<class T>
 class RingBuffer
 {
 private:
-	int m_pose;
-	int m_size;
-	T *m_values;
+	int m_pose; // position of an item of type t
+	int m_size; // the size of array of T
+	T *m_values; // dynamic allocation of T array
 
 public:
-	class Iterator;
+	class Iterator; // creating Iterator class
 
 public:
+	// RingBuffer constructor gets the size and initializes other values az 0 and null
 	RingBuffer(int size) : m_pose(0), m_size(size), m_values(NULL) {
 		m_values = new T[size];
 	}
 	
+	// RingBuffer destructor deallocates the values
 	~RingBuffer() {
 		delete [] m_values;
 
 	}
 
+	// fuction returns size
 	int size() {
 		return m_size;
 	}
 
+	// setting Iterator functions related to Ringbuffer : begin
 	Iterator begin() {
 		return Iterator(0, *this);
 	}
 
+	// setting Iterator functions related to Ringbuffer : end
 	Iterator end() {
 		return Iterator(m_size, *this);
 	}
 
+	// adding values to array
 	void add(T value) {
 
 		m_values[m_pose++] = value;
@@ -43,35 +50,43 @@ public:
 		}
 	}
 
+	// returning an " pose " value by ref
 	T& get(int pose) {
 		return m_values[pose];
 	}
 };
 
+// implementing " Iterator " class through T template
 template<class T>
 class RingBuffer<T>::Iterator {
 private:
-	int m_pose;
-	RingBuffer& m_Ring;
+	int m_pose; // current location
+	RingBuffer& m_Ring; // refrence to RingBuffer
+
 public:
+	// Iterator constructor initializing variables
 	Iterator(int pose, RingBuffer& Ring): m_pose(pose), m_Ring(Ring) {
 
 	}
 
+	// operator overloading: ++ for incrementing the iterator position or location
 	Iterator& operator++(int) {
 		m_pose++;
 		return *this;
 	}
 
+	// operator overloading: ++ for incrementing the iterator position or location
 	Iterator& operator++() {
 		m_pose++;
 		return *this;
 	}
 
+	// operator overloading: * inorder to get current value of Ringbuffer
 	T& operator*() {
 		return m_Ring.get(m_pose);
 	}
 
+	// operator overloading: != and == to check the Itrators
 	bool operator!=(const Iterator& other)const {
 		return m_pose != other.m_pose;
 	}
@@ -79,4 +94,5 @@ public:
 	bool operator==(const Iterator& other)const {
 		return m_pose == other.m_pose;
 	}
+
 };
--- Source.cpp
+++ Source.cpp
@@ -2,16 +2,21 @@
 
 int main(){
 
-
+	// Using template inorder to create a " RingBuffer " class, dowbelow using that class
 	RingBuffer<int> Buffer1(3);
 	Buffer1.add(1);
 	Buffer1.add(3);
 	Buffer1.add(2);
 	Buffer1.add(4);
+
+	// Iterating through Ringbuffer
 	for (RingBuffer<int>::Iterator it = Buffer1.begin(); it != Buffer1.end(); it++) {
 		cout << *it << endl;
 	}
+
 	cout << endl;
+
+	// adding to Ringbuffer
 	for (int i = 0; i != Buffer1.size(); i++) {
 		cout << Buffer1.get(i);
 	}
